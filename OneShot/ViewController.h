79e4893e628d9f60b5538bb50a6252136b750c02
Michael Wu <mikewu@tapgo.cc>
2015-08-08 02:31:54 +0800

    Initial Checkin for OneShot Checkin before using Pod install first time

---
+++ OneShot/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OneShot
+//
+//  Created by Michael Wu on 2015/8/6.
+//  Copyright (c) 2015年 tapgo Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ OneShot/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  OneShot
+//
+//  Created by Michael Wu on 2015/8/6.
+//  Copyright (c) 2015年 tapgo Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
